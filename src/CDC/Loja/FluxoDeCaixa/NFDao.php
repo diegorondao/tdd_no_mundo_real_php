ba0c5240b7438b393b23833950cd94e19639bb81
diegorondao <drondao@autodoc.com.br>
2018-01-04 11:46:48 -0200

    #CAP 8.2 - Mock Objects | PAG 120  | initiated

---
+++ src/CDC/Loja/FluxoDeCaixa/NFDao.php
@@ -0,0 +1,13 @@
+<?php 
+namespace CDC\Loja\FluxoDeCaixa;
+
+class NFDao
+{
+	public function persiste(NotaFiscal $nf)
+	{
+		// persiste NF
+	}
+
+}
+
+?>
---
+++ src/CDC/Loja/FluxoDeCaixa/SAP.php
@@ -0,0 +1,12 @@
+<?php 
+namespace CDC\Loja\FluxoDeCaixa;
+
+class SAP
+{
+	public function envia(NotaFiscal $nf)
+	{
+		//envia NF para o SAP
+	}
+
+}
+?>
