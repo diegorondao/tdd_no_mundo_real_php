0925c27d3502103cb1cbd4d0b98734c5621e1627
diegorondao <drondao@autodoc.com.br>
2018-01-04 13:50:31 -0200

    #CAP 8.5 - Classes estáveis | PAG 129  | initiated

--- src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
+++ src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
@@ -2,11 +2,22 @@
 
 namespace CDC\Loja\FluxoDeCaixa;
 
-use CDC\Loja\FluxoDeCaixa\Pedido;
+use CDC\Loja\FluxoDeCaixa\Pedido,
+	CDC\Loja\FluxoDeCaixa\NFDao,
+	CDC\Loja\FluxoDeCaixa\SAP;
 use DateTime;
 
 class GeradorDeNotaFiscal
 {
+	private $dao;
+	private $sap;
+
+	public function __construct(NFDao $dao, SAP $sap)
+	{
+		$this->dao = $dao;
+		$this->sap = $sap;
+	}
+
 	public function gera(Pedido $pedido)
 	{
 		return new NotaFiscal(
@@ -14,9 +25,8 @@ class GeradorDeNotaFiscal
 			$pedido->getValorTotal() * 0.94,
 			new DateTime()
 		);
-		
-		$nfDao = new NFDao();
-		if( $nfDao->persiste($nf) ) {
+
+		if( $this->dao->persiste($nf) && $this->sap->envia($nf) ){
 			return $nf;
 		}
 		return null;
--- tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
+++ tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
@@ -14,5 +14,36 @@ class GeradorDeNotaFiscalTest extends TestCase
 
 		$this->assertEquals(1000 * 0.94, $nf->getValor(), null, 0.00001);
 	}
+
+	public function testDevePersistirNFGerada()
+	{
+		$dao = Mockery::mock("CDC\Loja\FluxoDeCaixa\NFDao");
+		$dao->shouldReceive("persiste")->andReturn(true);
+		
+		$gerador = new GeradorDeNotaFiscal($dao);
+		$pedido = new Pedido("Andre", 1000, 1);
+		$nf = $gerador->gera($pedido);
+		
+		$this->assertTrue($dao->persiste());
+		$this->assertEquals(1000 * 0.94, $nf->getValor(), null, 0.00001);
+	}
+
+	public function testDeveEnviarNFGeradaParaSAP()
+	{
+		$dao = Mockery::mock("CDC\Loja\FluxoDeCaixa\NFDao");
+		$dao->shouldReceive("persiste")->andReturn(true);
+	
+		$sap = Mockery::mock("CDC\Loja\FluxoDeCaixa\SAP");
+		$sap->shouldReceive("envia")->andReturn(true);
+	
+		$gerador = new GeradorDeNotaFiscal($dao, $sap);
+		$pedido = new Pedido("Andre", 1000, 1);
+		
+		$nf = $gerador->gera($pedido);
+	
+		$this->assertTrue($sap->envia());
+		$this->assertEquals(1000 * 0.94,
+		$nf->getValor(), null, 0.00001);
+	}
 }
 ?>
